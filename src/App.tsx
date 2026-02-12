cef60cf51ac560d51608b682f52cbf8505537cb8
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-02-12 21:52:40 +0700

    added input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Input from "./components/input";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <Input label="Your Name" id="name" />
+      <Input label="Email" id="email" />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/input.tsx
@@ -0,0 +1,12 @@
+type InputProps = {
+  label: string;
+  id: string;
+};
+export default function Input({ label, id }: InputProps) {
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input type="text" id={id} />
+    </p>
+  );
+}
