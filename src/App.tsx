19cfc01983bfbbccc47c8e895ec84ee9326bc010
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-02-12 22:35:04 +0700

    added button with custom wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Button from "./components/Button";
 import Input from "./components/input";
 
 function App() {
@@ -5,6 +6,14 @@ function App() {
     <main>
       <Input label="Your Name" id="name" type="text" />
       <Input label="Email" id="email" type="email" />
+      <p>
+        <Button el="button">Submit</Button>
+      </p>
+      <p>
+        <Button el="anchor" href="https://google.com">
+          Linked
+        </Button>
+      </p>
     </main>
   );
 }
