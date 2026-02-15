a3a1d692e29850d555d3e0665aafcd50eb4c3f71
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-02-15 11:32:38 +0700

    added some component for advanced type-safe Context API & useReducer()

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import { useRef } from "react";
-import Button from "./components/Button";
-import Form, { type FormHandle } from "./components/Form";
-import Input from "./components/Input";
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
 
 function App() {
-  const customForm = useRef<FormHandle>(null);
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; email: string };
-    console.log(extractedData);
-    customForm.current?.clear();
-  }
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}>
-        <Input label="Your Name" id="name" type="text" />
-        <Input label="Email" id="email" type="email" />
-        <p>
-          <Button el="button">Submit</Button>
-        </p>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
