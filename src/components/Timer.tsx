738870a9207e266fecce2736e408258cbb4e7a31
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-04-09 07:33:38 +0700

    creating side effect

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { type Timer as TimerProps } from "../store/timer-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState<number | null>(
+    duration * 1000,
+  );
+
+  setInterval(function () {
+    setRemainingTime((prevTime) => prevTime - 50);
+  }, 50);
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress value={remainingTime} max={duration * 1000} />
+      </p>
+      <p>{}</p>
     </Container>
   );
 }
